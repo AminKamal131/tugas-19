1694f6417957419bb4dd4ed8660b0f0e8d576974
Amin Kamal <aminkamal355@gmail.com>
2022-12-18 21:40:26 +0800

    tugas 19
    tugas 19

---
+++ tugas19.js
@@ -0,0 +1,16 @@
+function rr(){
+    var ak = "Semangat..."
+    console.log(ak.repeat(5))
+    console.log("=======================================")
+    console.log("BINTANG")
+    console.log('*'.repeat(5));
+    console.log('*'.repeat(20));
+    console.log('*'.repeat(40));
+    console.log('*'.repeat(80));
+    console.log('*'.repeat(40));
+    console.log('*'.repeat(20));
+    console.log('*'.repeat(5));
+}
+rr()
+
+
